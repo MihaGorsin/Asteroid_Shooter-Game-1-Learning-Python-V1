8a74569f73ae7d997a6d0a652822dbba14621003
Miha Gorsin <miha.gorsin@gmail.com>
2026-02-26 23:50:25 +0100

    Dodal grafike se učil delati z surfacy

--- main.py
+++ main.py
@@ -9,9 +9,26 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # Ime, ki bo na okviru okna
 pygame.display.set_caption('Neteor shooter')
 
+
+#za FPS
+clock = pygame.time.Clock()
+
 # Surface je grafična površina
-test_surface = pygame.Surface((400,100))
+#test_surface = pygame.Surface((400,100))
 # Surface rabimo pritrditi na display surface
+
+# import font
+font = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 50)
+# Ta false je aa antyaliesing ali nekiaj - gre za robove
+text_surf = font.render('Ladja', False, (255,255,255))
+
+#Rabiš convertat v convert ali convert alpha
+ship_surf = pygame.image.load('./Grafike/Ladja-1.png').convert_alpha()
+ozadje = pygame.image.load('./Grafike/Ozadje.png').convert()
+
+
+# Test animacije ladje
+x_za_ladjo = 700
 
 while True: # To bo v neskončnost loopalo
 
@@ -21,7 +38,17 @@ while True: # To bo v neskončnost loopalo
             pygame.quit()
             sys.exit()
 
+    # Frame limit
+    clock.tick(60)
+
     # 2. updates
+    display_surface.fill('dark gray')
+    display_surface.blit(ozadje)
+    x_za_ladjo -= 1 
+    display_surface.blit(ship_surf,(300,x_za_ladjo))
+    display_surface.blit(text_surf,((WINDOW_WIDTH /2) - 40 ,10))
+    
+    
 
     # 3.show the frame to the player / update the display surface
     pygame.display.update()
