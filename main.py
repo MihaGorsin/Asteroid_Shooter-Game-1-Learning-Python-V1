2e7c3672452db2ae8057765f578b82c6cf35861a
Miha Gorsin <miha.gorsin@gmail.com>
2026-03-01 21:03:58 +0100

    nekaj popravkov in rectangels

--- main.py
+++ main.py
@@ -21,14 +21,18 @@ clock = pygame.time.Clock()
 font = pygame.font.Font('PixelifySans-VariableFont_wght.ttf', 50)
 # Ta false je aa antyaliesing ali nekiaj - gre za robove
 text_surf = font.render('Ladja', False, (255,255,255))
+text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH/2, WINDOW_HEIGHT - 80))
 
 #Rabiš convertat v convert ali convert alpha
 ship_surf = pygame.image.load('./Grafike/Ladja-1.png').convert_alpha()
+ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+print(ship_rect)
+
 ozadje = pygame.image.load('./Grafike/Ozadje.png').convert()
 
-
+smer_premika = 3
 # Test animacije ladje
-x_za_ladjo = 700
+
 
 while True: # To bo v neskončnost loopalo
 
@@ -37,16 +41,22 @@ while True: # To bo v neskončnost loopalo
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    ship_rect.x -= smer_premika
+    if ship_rect.x < 0:
+        smer_premika = -3
 
+    if ship_rect.x > 350:
+        smer_premika = 3
     # Frame limit
     clock.tick(60)
+
+
 
     # 2. updates
     display_surface.fill('dark gray')
     display_surface.blit(ozadje)
-    x_za_ladjo -= 1 
-    display_surface.blit(ship_surf,(300,x_za_ladjo))
-    display_surface.blit(text_surf,((WINDOW_WIDTH /2) - 40 ,10))
+    display_surface.blit(ship_surf,(ship_rect))
+    display_surface.blit(text_surf,text_rect)
     
     
 
